edb979ff30772899da03bc98ebd7d5e9ba301c04
jon4god <jon4god@mail.ru>
2015-11-11 20:57:06 +0300

    Работающая версия

--- eduminrus.php
+++ eduminrus.php
@@ -1,28 +1,39 @@
 <?php
 /*
-Plugin Name: eduminrus
-Plugin URI: https://github.com/jon4god/wp-yandex-addurl
-Description: A simple plugin that adds a widget to the admin panel to add and verify the site links to search engine.
+Plugin Name: Теги для ВУЗов
+Plugin URI: https://github.com/jon4god/eduminrus-editor-button
+Description: Плагин для редактора TinyMCE, который добавляет для сайта теги в соотвествии с рекомендациями Министерства Образования.
 Version: 0.1
 Author: Evgeniy Kutsenko
 Author URI: http://starcoms.ru
 License: GPL2
 */
 if (!function_exists('add_action')) {
-    echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
-    exit;
+  echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
+  exit;
+}
+
+add_filter('tiny_mce_before_init', 'eduminrus_mce_options');
+function eduminrus_mce_options( $init ) {
+  $ext = 'span[id|name|class|lang|style|itemprop]';
+  if ( isset( $init['extended_valid_elements'] ) ) {
+    $init['extended_valid_elements'] .= ',' . $ext;
+  } else {
+    $init['extended_valid_elements'] = $ext;
+   }
+  return $init;
 }
 
 add_action( 'mce_buttons', 'eduminrus_register_buttons' );
 function eduminrus_register_buttons($buttons) {
-	array_push( $buttons, 'eduminrus' );
-	return $buttons;
+  array_push( $buttons, 'eduminrus' );
+  return $buttons;
 }
 
 add_action( 'mce_external_plugins', 'eduminrus_add_button' );
 function eduminrus_add_button($plugin_array) {
-	$plugin_array['eduminrus'] = plugins_url('/js/eduminrus-plugin.js',__FILE__);
-	return $plugin_array;
+  $plugin_array['eduminrus'] = plugins_url('/js/eduminrus-plugin.js',__FILE__);
+  return $plugin_array;
 }
 
 add_action('admin_enqueue_scripts', 'eduminrus_add_css');
--- js/eduminrus-plugin.js
+++ js/eduminrus-plugin.js
@@ -1,66 +1,53 @@
 (function() {
-    tinymce.create('tinymce.plugins.eduminrus', {
+  tinymce.create('tinymce.plugins.eduminrus', {
 
-       init : function(ed, url) {
-            ed.addButton('eduminrus', {
-                title : 'Вставка кода МинОбр России',
-                icon: 'icon dashicons-welcome-learn-more',
-                onclick: function() {
-                		ed.windowManager.open({
-                    title: 'Выберите тип опубликованных данных',
-                    body: [{
-                        type: 'listbox',
-                        name: 'className',
-                        label: 'Тип',
-                        values: [{
-                            text: 'Дата создания образовательной организации',
-                            value: 'RegDate'
-                        }, {
-                            text: 'Информация о месте нахождения образовательной организации',
-                            value: 'Address'
-                        }, {
-                            text: 'Информация о месте нахождения филиалов образовательной организации (при наличии)',
-                            value: 'AddressFil'
-                        }, {
-                            text: 'Информация о режиме и графике работы образовательной организации',
-                            value: 'WorkTime'
-                        }]
-                    }, ],
-                    onsubmit: function(e) {
-/*
-	                    var selected_text = ed.selection.getContent();
-	                    var return_text = '';
-											return_text = '<span itemprop=&quot;' + e.data.className + '&quot;>' + selected_text + '</span>';
-											ed.execCommand('mceInsertContent', false, return_text);
-*/
-											ed.insertContent(
-                            '<span itemprop=&quot;' +
-                            e.data.className +
-                            '&quot;>' +
-                            ed.selection
-                            .getContent() +
-                            '</span>'
-                        );
-                    }
-                });
-            	}
-            });
-        },
-
-        createControl : function(n, cm) {
-            return null;
-        },
-
-        getInfo : function() {
-            return {
-                longname : 'Eduminrus Button',
-                author : 'Evgeniy Kutsenko',
-                authorurl : 'http://starcoms.ru/',
-                infourl : 'http://wiki.moxiecode.com/index.php/TinyMCE:Plugins/example',
-                version : "0.1"
-            };
+    init : function(ed, url) {
+      ed.addButton('eduminrus', {
+        title : 'Вставка кода МинОбр России',
+        icon: 'icon dashicons-welcome-learn-more',
+        onclick: function() {
+          ed.windowManager.open({
+          title: 'Выберите тип опубликованных данных',
+          body: [{
+            type: 'listbox',
+            name: 'className',
+            label: 'Тип',
+            values: [{
+                text: 'Дата создания образовательной организации',
+                value: 'RegDate'
+              }, {
+                text: 'Информация о месте нахождения образовательной организации',
+                value: 'Address'
+              }, {
+                text: 'Информация о месте нахождения филиалов образовательной организации (при наличии)',
+                value: 'AddressFil'
+              }, {
+                text: 'Информация о режиме и графике работы образовательной организации',
+                value: 'WorkTime'
+              }]
+            }, ],
+          onsubmit: function(e) {
+            ed.insertContent('<span itemprop=' + e.data.className + ' >' + ed.selection.getContent() + '</span>');
+          }
+          });
         }
-    });
+      });
+    },
 
-    tinymce.PluginManager.add( 'eduminrus', tinymce.plugins.eduminrus );
+    createControl : function(n, cm) {
+      return null;
+    },
+
+    getInfo : function() {
+      return {
+        longname : 'Eduminrus Button',
+        author : 'Evgeniy Kutsenko',
+        authorurl : 'http://starcoms.ru/',
+        infourl : 'https://github.com/jon4god/eduminrus-editor-button',
+        version : "0.1"
+      };
+    }
+  });
+
+tinymce.PluginManager.add( 'eduminrus', tinymce.plugins.eduminrus );
 })();
